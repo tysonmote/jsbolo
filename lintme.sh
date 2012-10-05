0514852689491c53b718c2ed2a8d77b908774482
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2012-10-05 13:34:41 -0400

    added lintme.sh and updated .gitignore

---
+++ lintme.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+clear && ~/bin/jslint.js js/bolo.js js/bmapstring.js js/camera.js js/rect.js js/tilemap.js js/client.js js/clientui.js js/debug.js js/parse.js js/vector.js js/load.js js/webserver.js js/web.js js/server.js js/websocket.js js/bmapfile.js
